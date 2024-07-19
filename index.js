26153972780edb20f5a516d76021eb62c16750d2
Viggen <koriremanuel0@gmail.com>
2024-07-19 09:28:29 +0300

    Array Update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Solution
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce(function (accumulator, element) {
+    return accumulator + element;
+}, 0);
+
+
+
